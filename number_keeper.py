7da2897fc06b16d2c0c34db9106c162500deb2fe
Christian Höltje <docwhat@gerf.org>
2025-07-05 21:47:28 -0400

    Add files via upload

---
+++ number_keeper.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+class NumberKeeper:
+    def __init__(self, initial_number:int =None):
+        "Optionally, sets kept number on creation."
+        self.number = initial_number
+
+    def set(self, new_number: int) -> None:
+        "Set the kept number, overwriting the previous value."
+        self.number = new_number
+
+    def get(self) -> int:
+        "Returns the kept number."
+        return self.number
+
+    def __str__(self) -> str:
+        return f"Kept Value: {repr(self.number)}"
+
+if __name__ == "__main__":
+    z = NumberKeeper()
+    print(f"Ready!")
---
+++ property_decorator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import unittest
+
+class Animal:
+    def __init__(self, name, species, weight):
+        self.name = name
+        self.species = species
+        self.weight = weight
+
+class TestAnimal(unittest.TestCase):
+    def example(self):
+        return Animal("Oscar", "Dog", 10)
+
+    def test_name(self):
+        oscar = self.example()
+        oscar.name = "Oscar"
+        self.assertEqual(oscar.name, "Oscar")
+
+    def test_species(self):
+        oscar = self.example()
+        oscar.species = "doggy"
+        self.assertEqual(oscar.species, "doggy")
+
+    def test_weight(self):
+        oscar = self.example()
+        oscar.weight = 10
+        self.assertEqual(oscar.weight, 10)
+
+if __name__ == "__main__":
+    unittest.main()
